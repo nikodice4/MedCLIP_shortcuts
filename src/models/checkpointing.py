92897c2a35cb165d31a7c76d61d0855d023b07d5
Nikolette Zoe Pedersen <nizp@itu.dk>
2026-03-25 13:23:06 +0100

    Update checkpointing.py

--- src/models/checkpointing.py
+++ src/models/checkpointing.py
@@ -2,15 +2,17 @@
 
 import torch
 
-def checkpoint(model, optimiser, epoch, filename):
+def checkpoint(model, optimiser, epoch, filename, best_val_loss=None, es_count=None):
     torch.save({
         'model': model.state_dict(),
         'optimiser': optimiser.state_dict(),
         'epoch': epoch,
+        'best_val_loss': best_val_loss,
+        'es_count': es_count,
     }, filename)
     
 def resume(model, optimiser, filename):
     checkpoint = torch.load(filename)
     model.load_state_dict(checkpoint['model'])
     optimiser.load_state_dict(checkpoint['optimiser'])
-    return checkpoint['epoch']
+    return checkpoint['epoch'], checkpoint.get('best_val_loss', float('inf')), checkpoint.get('es_count', 0)
